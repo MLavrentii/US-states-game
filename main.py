5ab31c06d358d8a5db6e28cdca09cedd018a004b
MLavrentii <71477362+mlavrentii@users.noreply.github.com>
2023-03-10 23:05:58 +0900

    Create python-package.yml

--- main.py
+++ main.py
@@ -12,38 +12,43 @@ state = turtle.Turtle()
 state.hideturtle()
 state.penup()
 state.speed(0)
+state.color("black")
 
-def draw_country(pos, state):
+def draw_country(pos, answer):
     state.goto(pos)
-    state.write(arg=f"{state}")
+    state.write(arg=f"{answer}")
 
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
 #
 # turtle.onscreenclick(get_mouse_click_coor)
 
-
-answer_state = screen.textinput(title="Guess the State", prompt="What's another stat's name?").capitalize()
-print(answer_state)
-
-data = pandas.read_csv("50_states.csv")
-states_list = data.state.to_list()
+new_data = pandas.read_csv("50_states.csv")
+states_list = new_data.state.to_list()
 
 
+called_states = []
+
+while len(called_states) < len(states_list):
+    answer_state = screen.textinput(title=f"Guess the State {len(called_states)}/{len(states_list)}", prompt="What's another stat's name?").title()
+    if answer_state == "Exit":
+        break
+    if answer_state in states_list:
+        called_states.append(answer_state)
+        found_state = new_data[new_data.state == answer_state]
+        pos = (int(found_state["x"]), int(found_state["y"]))
+        print(pos)
+        draw_country(pos, answer_state)
+
+# states_to_learn.csv
+to_learn = states_list
+for state in called_states:
+    to_learn.remove(state)
+new_data = pandas.DataFrame(to_learn)
+new_data.to_csv("states_to_learn.csv")
 
 
-print(states_list)
-if answer_state in states_list:
-    found_state = data[data.state == answer_state]
-    pos = (found_state[x], found_state[y])
-    print(pos)
-    # draw_country(pos, answer_state)
 
-
-
-turtle.mainloop()   # it's way to stay open our screen until our code is running
-
-
-
+# turtle.exitonclick()   # it's way to stay open our screen until our code is running
 # screen.exitonclick()
 
